02a8bc24e897f8860fd6ac853a08f86e758b2191
andreikostsen <pks@tut.by>
2024-02-23 13:33:47 +0300

    feat: add button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { Button } from '@/components/ui/button'
+
+import s from './components/ui/button/button.module.scss'
+
 function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      Hello
+      <div>
+        <Button className={s.button}>Button As Button</Button>
+      </div>
+      <div>
+        <Button as={'a'} className={s.button} href={'https://my.beltelecom.by/login'}>
+          Button As a
+        </Button>
+      </div>
+    </div>
+  )
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-
 import './styles/index.scss'
 
 import App from './App'
